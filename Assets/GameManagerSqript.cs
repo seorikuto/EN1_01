4507093ed1f0f7adc3676a1d95e53084dad3f123
k022g1032@g.neec.ac.jp <k022g1032@g.neec.ac.jp>
2023-04-27 14:44:01 +0900

    プロジェクト ファイルを追加します。

---
+++ Assets/GameManagerSqript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerSqript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //デバッグログ
+        Debug.Log("Hello World");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
